9675beb7d3620675eafebb9310b3dd71cc5c2e60
Mark Pecci <mark_pecci@redlands.edu>
2023-09-17 20:28:23 -0700

    Assignment 3

--- Homework 3/src/pecci/SalesItem.java
+++ Homework 3/src/pecci/SalesItem.java
@@ -1,5 +1,61 @@
 package pecci;
 
 public class SalesItem {
+	private String item;
+	private double price;
+	private int quantity; 
+	private double total;
 
+	public SalesItem(String item, double price, int quantity) {
+		this.item = item;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String i) {
+		item = i;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double p) {
+		price = p;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int q) {
+		quantity = q;
+	}
+
+	public double calcTotal() {
+		return quantity * price;
+	}
+
+	public String toString() {
+		String output = item;
+		for(int i = 0; i < 15 - item.length(); i++ ) {
+			output += " ";
+		}
+		output += "$";
+		if(price < 10.0) {
+			output += "0" + price;
+		}
+		else {
+			output += price;
+		}
+		for(int j = 0; j < 5; j++) {
+			output += " ";
+		}
+		output += quantity;
+		return output;
+	}
 }
--- Homework 3/src/pecci/SalesSlip.java
+++ Homework 3/src/pecci/SalesSlip.java
@@ -1,5 +1,47 @@
 package pecci;
 
-public class SalesSlip {
+import java.util.ArrayList;
 
+public class SalesSlip {
+	private String output = "";
+	ArrayList<SalesItem> slip;
+	
+	public SalesSlip() {
+		 slip = new ArrayList<SalesItem>();
+	}
+
+	public void addSalesItem(SalesItem s) {
+		slip.add(s);
+	}
+
+	public String computeTotal() {
+		double total = 0;
+		if(slip.size() >= 1) {
+			for(int i = 0; i < slip.size(); i++) {
+				total += slip.get(i).getPrice() * slip.get(i).getQuantity();
+			}
+		}
+		else {
+			output = "Please enter some sales items";
+			return output;
+		}
+		if(total < 10.00) {
+			output = "$0" + String.format("%.2f", total);
+			return output;
+		}
+		output = "$" + String.format("%.2f", total);
+		return output;
+	}
+
+	public String toString() {
+		String output = "";
+		if(slip.size() < 1) {
+			output = "Please enter some sales items";
+			return output;
+		}
+		for (int i = 0; i < slip.size(); i++){
+			output += slip.get(i).toString() + "\n";
+		}
+		return output;
+	}
 }
